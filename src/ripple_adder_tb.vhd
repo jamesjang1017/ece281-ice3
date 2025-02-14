4bf23c0a17f8d96c440d1900d364c305ca05af46
jamesjang1017 <jjangjangjaewon@gmail.com>
2025-02-13 22:13:58 -0700

    submit

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,7 +49,10 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"E" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
-	
+	   w_addends <= x"0F"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"0" and w_Cout = '1') report "bad with zeros plus F with cin" severity failure;
+	   w_addends <= x"FF"; w_Cin <= '0'; wait for 10 ns;
+	       assert (w_sum = x"0" and w_Cout = '0') report "bad with F plus F without cin" severity failure;
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
